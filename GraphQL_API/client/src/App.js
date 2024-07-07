35cab1abcc2a7d94d4a35f3e5c27b6930eedab2b
MikeBilbee <cmbilbee@gmail.com>
2024-07-07 12:45:09 -0500

    9.0 now

--- GraphQL_API/client/src/App.js
+++ GraphQL_API/client/src/App.js
@@ -1,28 +1,28 @@
-/*import ApolloClient from 'apollo-boost';
-/*
-...
-*/
-// components
+// Import ApolloClient from 'apollo-boost';
+
+import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 import TaskList from './components/TaskList';
 import AddTask from './components/AddTask';
 import AddProject from './components/AddProject';
-// apollo client setup
-/*
-...
-*/
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/graphql'
+});
+
+
 function App() {
-  return ( <
-    div id = "main" >
-    <
-    div id = "bg" > < /div> <
-    h1 > Holberton school tasks < /h1> <
-    TaskList / >
-    <
-    AddProject / >
-    <
-    AddTask / >
-    <
-    /div>
+  return (
+    <ApolloProvider client={client}>
+      <div id = "main">
+        <div id = "bg"></div> 
+        <h1 > Holberton school tasks </h1> 
+        <TaskList/>
+        <AddProject/>
+        <AddTask/>
+      </div>
+    </ApolloProvider>
   );
 }
 
